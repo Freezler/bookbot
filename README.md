502cf64ecc19fc3d4f4247a5749309170726bf94
Freezler <r.l.devries@live.nl>
2024-06-27 20:44:33 +0200

    Implement bookbot features including word and vowel count

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-## bookbot
 
-### Welcome to Bookbot: 
+### BookBot is my first python project!
+
+# Welcome to Bookbot: 
 
 ###### a Python program that can analyze an entire book and print out an interesting statistical report.
-
-###### BookBot is my first project!
---
+++ main.py
@@ -0,0 +1,36 @@
+def read_book(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+        return None
+
+
+def count_words(book):
+    words = book.split()
+    num_words = len(words)
+    return num_words
+
+
+def main():
+    number_of_vowels = 0
+    book_path = "/Users/randydevries/workspace/github.com/FREEZLER/bookbot/books/frankenstein.txt"
+    book = read_book(book_path)
+
+    if book is not None:
+        num_words = count_words(book)
+        my_string = f"The book has {num_words} words."
+        lowered_string = book
+        print(my_string.lower())
+
+        for char in lowered_string:
+            if char in "aAeEiIoOuU":
+                number_of_vowels += 1
+        print(f"Number of vowels: {number_of_vowels}")
+    else:
+        print("Failed to read the book.")
+
+
+if __name__ == "__main__":
+    main()
